ef424e81a1402647b5afe77ff6eb51e8d8f4a64a
Ron Natarajan <ronnatarajan@pal-nat186-1-34.itap.purdue.edu>
2023-09-16 10:47:43 -0400

    Basic application file added

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    print("pp")
